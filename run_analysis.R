56549708781b93170a1d4ae8acd6178d9f93100c
07engineer <djchapman@gmail.com>
2014-09-18 11:39:43 -0700

    more modifications
    still learning github, moved wrong version

--- run_analysis.R
+++ run_analysis.R
@@ -6,22 +6,17 @@
 #First, read in data files
 ###########################
 
-setwd("~/coursera/getting and cleaning data/course project")
 rm(list = ls())
 activity_labels <- read.table("activity_labels.txt")
 features <- read.table("features.txt")
 
-setwd("~/coursera/getting and cleaning data/course project/test")
 X_test <- read.table("X_test.txt")
 y_test <- read.table("y_test.txt")
 subject_test <- read.table("subject_test.txt")
 
-setwd("~/coursera/getting and cleaning data/course project/train")
 X_train <- read.table("X_train.txt")
 y_train <- read.table("y_train.txt")
 subject_train <- read.table("subject_train.txt")
-
-setwd("~/coursera/getting and cleaning data/course project")
 
 #######################
 # Combine data 
